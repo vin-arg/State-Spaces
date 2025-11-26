27376b0133b630533b5efcff72fe2762d8f90bce
vin-arg <vinceashanti@gmail.com>
2025-11-27 02:21:01 +0800

    .

--- statespaces/home/models.py
+++ statespaces/home/models.py
@@ -77,3 +77,107 @@ class Reservation(models.Model):
 
 
 # Create your models here.
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import User
+
+
+# class Home(models.Model):
+#     name = models.CharField(max_length=200)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class Building(models.Model):
+#     building_id = models.CharField(max_length=200, primary_key=True)
+#     name = models.CharField(max_length=200)
+#     address = models.CharField(max_length=300)
+#     city = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class Amenity(models.Model):
+#     amenity_id = models.CharField(max_length=20, primary_key=True)
+#     type = models.CharField(max_length=200)
+#     description = models.TextField(blank=True)
+
+#     def __str__(self):
+#         return self.type
+
+
+# class Venue(models.Model):
+#     venue_id = models.CharField(max_length=20, primary_key=True)
+#     name = models.CharField(max_length=200)
+#     capacity = models.PositiveIntegerField()
+#     venue_type = models.CharField(max_length=100)
+#     floor_area = models.PositiveIntegerField()
+#     building = models.ForeignKey(
+#         Building,
+#         on_delete=models.CASCADE,
+#         to_field="building_id"
+#     )
+#     floor = models.CharField(max_length=20)
+#     under_renovation = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class VenueAmenity(models.Model):
+#     venue = models.ForeignKey(
+#         Venue,
+#         on_delete=models.CASCADE,
+#         to_field="venue_id"
+#     )
+#     amenity = models.ForeignKey(
+#         Amenity,
+#         on_delete=models.CASCADE,
+#         to_field="amenity_id"
+#     )
+#     quantity = models.PositiveIntegerField(default=1)
+
+
+# class Agent(models.Model):
+#     agent_id = models.CharField(max_length=20, primary_key=True)
+#     name = models.CharField(max_length=200)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class Customer(models.Model):
+#     customer_id = models.CharField(max_length=20, primary_key=True)
+#     customer_name = models.CharField(max_length=200)
+#     birth_date = models.DateField()
+#     location = models.CharField(max_length=200)
+
+#     def __str__(self):
+#         return self.customer_name
+
+
+# class Reservation(models.Model):
+#     reservation_id = models.CharField(max_length=20, primary_key=True)
+#     venue = models.ForeignKey(
+#         Venue,
+#         on_delete=models.CASCADE,
+#         to_field="venue_id"
+#     )
+#     customer = models.ForeignKey(
+#         Customer,
+#         on_delete=models.CASCADE,
+#         to_field="customer_id"
+#     )
+#     agent = models.ForeignKey(
+#         Agent,
+#         on_delete=models.CASCADE,
+#         to_field="agent_id"
+#     )
+#     number_of_participants = models.PositiveIntegerField()
+#     start_datetime = models.DateTimeField()
+#     end_datetime = models.DateTimeField()
